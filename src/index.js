afd67afdc1d916b867e4df785485ae08ebb3db10
Natthaphon Phanit <natthaphon.phanit@allianz.com>
2018-06-14 16:37:34 +0700

    update eslint rules

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDaKhj3bfyKuNG6YyR0cVKQq016lfjz93M';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+YTSearch({ key: API_KEY, term: 'surfboards' }, data => console.log(data));
+
+const App = () => (
+  <div>
+    <SearchBar />
+  </div>
+);
 
 ReactDOM.render(<App />, document.querySelector('.container'));
